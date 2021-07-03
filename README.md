ea365d0b91cd2b52d750f45261d70f5621a4db0c
ltrzalez <ltrzalez@gmail.com>
2021-07-03 15:04:57 -0300

    added firebase & some txt in readme

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # todo-list
 
-makeing a todo list app with react and firebase
+making a todo list app with react and firebase
 
 # deployed app
 
     https://todo-list-454d2.web.app/
+
+# The goal
+
+Everybody need sing in to access to private todo list.
+
+# some reminders for me to achiave the goal
+
+CRUD of todo tickets, conditional rendering wiht In/out permission
--- src/App.js
+++ src/App.js
@@ -1,4 +1,22 @@
 import './App.css';
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+
+// var firebaseConfig = {
+//     apiKey: "AIzaSyC1hdtq8L9NxIGKWF_zL5JbpvBEkGwp4NM",
+//     authDomain: "todo-list-454d2.firebaseapp.com",
+//     projectId: "todo-list-454d2",
+//     storageBucket: "todo-list-454d2.appspot.com",
+//     messagingSenderId: "351192633254",
+//     appId: "1:351192633254:web:0d72de9ede6441eafb0967",
+//     measurementId: "G-SJB0LNBMH4"
+// };
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
+
 
 function App() {
   return (
